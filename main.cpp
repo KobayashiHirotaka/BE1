5eb24300504bbb6e0c34deb858a3809b085dd125
KobayashiHirotaka <K022G0013@g.neec.ac.jp>
2025-06-18 13:05:44 +0900

    UTF-8の関数を追加

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <windows.h>
+#include <string>
 #include "httpClient.h"
+
+std::wstring ConvertString(const std::string& s)
+{
+	int len = MultiByteToWideChar(CP_UTF8, 0, s.data(), (int)s.size(), NULL, 0);
+
+	std::wstring ws(len, 0);
+
+	MultiByteToWideChar(CP_UTF8, 0, s.data(), (int)s.size(), &ws[0], len);
+
+	return ws;
+}
+
+void WriteUtf8(const std::string& s)
+{
+	std::wstring ws = ConvertString(s);
+
+	DWORD _;
+
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), ws.c_str(), (DWORD)ws.size(), &_, nullptr);
+}
 
 int main()
 {
@@ -13,11 +34,11 @@ int main()
 		if (future.valid() && future.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
 		{
 			std::string res = future.get();
-			std::cout << "PATCH結果: " << res << std::endl;
+			WriteUtf8("PATCH結果:" + res + "\n");
 
 			auto getFuture = GetAllFacultiesAsync();
 			std::string all = getFuture.get();
-			std::cout << "現在の全学科一覧:\n" << all << std::endl;
+			WriteUtf8("現在の全学科一覧:\n" + all + "\n");
 
 			break;
 		}
